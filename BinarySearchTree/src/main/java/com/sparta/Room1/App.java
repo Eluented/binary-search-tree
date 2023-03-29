6cc9f59fea00e76216553e15cc48c64bdc7580c0
HamzaKazi <hamzakaziii1@gmail.com>
2023-03-29 16:35:14 +0100

    Few changes to names

--- BinarySearchTree/src/main/java/com/sparta/Room1/App.java
+++ BinarySearchTree/src/main/java/com/sparta/Room1/App.java
@@ -1,4 +1,4 @@
-package com.sparta.anu;
+package com.sparta.Room1;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
